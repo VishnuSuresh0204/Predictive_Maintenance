fa8d152600a8885719c00ef1f0edb4e5d16b3404
Carbon Credits Developer <developer@carbon-credits.com>
2026-09-17 15:10:54 +0530

    Update static files configuration
    Commit the settings update separately.

--- maintain/maintain/settings.py
+++ maintain/maintain/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+0(c9$wpps1%std^m$8*n2d32et18-7y+45iy=7asz9zhir3+i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
